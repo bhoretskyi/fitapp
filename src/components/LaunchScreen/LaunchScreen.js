a699b4f667e848694eaa6960ede4c1bddb964216
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2025-04-12 21:12:56 +0200

    add second launch screen

--- src/components/LaunchScreen/LaunchScreen.js
+++ src/components/LaunchScreen/LaunchScreen.js
@@ -1,11 +1,16 @@
-import logo from '../../img/launch_1.png'
-import { LaunchScreenContainer } from './LaunchcreenStyled'
+import logo from "../../img/launch_1.png";
+import { LaunchScreenContainer, AppHeroText, GreenText, LaunchText, LaunchStartBtn } from "./LaunchcreenStyled";
 
 export const LaunchScreen = () => {
-    return (
-
-        <LaunchScreenContainer className='container'>
-        <img src={logo} alt="img" width={217}/>
-      </LaunchScreenContainer>
-    )
-}
+  return (
+    <LaunchScreenContainer className="container">
+      <img src={logo} alt="img" width={217} hidden />
+      <img src={logo} alt="img" width={313} />
+      <AppHeroText>
+        PRO <GreenText>FITNESS</GreenText>
+      </AppHeroText>
+      <LaunchText>We train your body to be great and fit.</LaunchText>
+      <LaunchStartBtn type="button">Let’s Start</LaunchStartBtn>
+    </LaunchScreenContainer>
+  );
+};
--- src/components/LaunchScreen/LaunchcreenStyled.js
+++ src/components/LaunchScreen/LaunchcreenStyled.js
@@ -1,12 +1,53 @@
 import styled from "styled-components";
 export const LaunchScreenContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-padding-top: 241px;
-padding-right: 73px;
-padding-left: 73px;
-padding-bottom: 315px                       ;
-background: linear-gradient(to bottom right,rgb(253, 253, 141),rgb(255, 255, 255),rgb(223, 241, 134));
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 241px;
+  padding-right: 73px;
+  padding-left: 73px;
+  padding-bottom: 315px;
+  background: linear-gradient(
+    to bottom right,
+    rgb(253, 253, 141),
+    rgb(255, 255, 255),
+    rgb(223, 241, 134)
+  );
+  height: 100%;
+`;
 
+export const AppHeroText = styled.h1`
+font-family: "Bebas Neue";
+font-size: 48px;
+font-weight: 400;
+line-height: 58px;
+letter-spacing: -0.5px;
+text-align: center;
+
+`
+export const GreenText = styled.span`
+color: var(--primary-color);
+`
+
+export const LaunchText = styled.p`
+font-family: "DM Sans";
+font-size: 16px;
+font-weight: 400;
+line-height: 21px;
+letter-spacing: -0.5px;
+text-align: center;
+`
+
+export const LaunchStartBtn = styled.button`
+border-radius: 5px;
+padding:  16px 50px ;
+
+background: rgb(25, 25, 25);
+color: rgb(255, 255, 255);
+font-family: "Bebas Neue";
+font-size: 20px;
+font-weight: 400;
+line-height: 24px;
+letter-spacing: 0.5px;
+text-align: center;
 `
